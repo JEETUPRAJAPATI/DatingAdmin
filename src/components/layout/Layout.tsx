9597eddd3126e3ccd679dd95b49fe8700bcd5486
jeetu-mj <jeetu.mj123@gmail.com>
2025-04-08 12:18:50 +0530

    Update layout

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -41,9 +41,7 @@ export function Layout() {
         </header>
 
         <main className="flex-1 overflow-auto p-4 md:p-6">
-          <div className="mx-auto max-w-7xl">
-            <Outlet />
-          </div>
+          <Outlet />
         </main>
       </div>
     </div>
